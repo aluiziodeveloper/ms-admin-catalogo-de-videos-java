65af0ed0880cbb9242aa7c851ed899dddfd61f3a
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-04-28 12:31:58 -0300

    Implementing either pattern

--- application/src/main/java/br/com/aluiziodeveloper/admin/catalogo/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/br/com/aluiziodeveloper/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,7 +1,9 @@
 package br.com.aluiziodeveloper.admin.catalogo.application.category.create;
 
 import br.com.aluiziodeveloper.admin.catalogo.application.UseCase;
+import br.com.aluiziodeveloper.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
 public abstract class CreateCategoryUseCase
-        extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+        extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 }
