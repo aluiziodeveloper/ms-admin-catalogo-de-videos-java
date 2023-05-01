cf24df6ef8ae09f210ee34abd0288d10005f5fa8
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-05-01 15:48:24 -0300

    Creating the strategy for integrated tests with database

---
+++ infrastructure/src/test/java/br/com/aluiziodeveloper/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,23 @@
+package br.com.aluiziodeveloper.admin.catalogo.infrastructure.category;
+
+import br.com.aluiziodeveloper.admin.catalogo.infrastructure.MySQLGatewayTest;
+import br.com.aluiziodeveloper.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MySQLGatewayTest
+public class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private CategoryMySQLGateway categoryGateway;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testInjectedDependencies() {
+        Assertions.assertNotNull(categoryGateway);
+        Assertions.assertNotNull(categoryRepository);
+    }
+}
