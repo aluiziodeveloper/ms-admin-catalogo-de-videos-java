267967a93931c6c6a6c424e819996db1195e00f1
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-05-01 11:16:41 -0300

    TDD get category usecase

---
+++ application/src/main/java/br/com/aluiziodeveloper/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,7 @@
+package br.com.aluiziodeveloper.admin.catalogo.application.category.retrieve.get;
+
+import br.com.aluiziodeveloper.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase
+        extends UseCase<String, CategoryOutput> {
+}
