7a575978f5c1ebde0cade48867d4d7c472f6103e
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-05-01 13:22:11 -0300

    Adding SpringBoot to the infrastructure layer

--- infrastructure/src/main/java/br/com/aluiziodeveloper/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/br/com/aluiziodeveloper/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,13 @@
 package br.com.aluiziodeveloper.admin.catalogo.infrastructure;
 
+import br.com.aluiziodeveloper.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/br/com/aluiziodeveloper/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.com.aluiziodeveloper.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.aluiziodeveloper.admin.catalogo")
+public class WebServerConfig {
+}
